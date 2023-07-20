f112a3122afc8cf8ccbaf51983574c9b72a2368e
Gropacha <jacquesandre.bourdoiseau@gmail.com>
2023-07-20 11:25:56 +0200

    Initial commit

---
+++ app/src/main/java/fr/doranco/flash/s1tp7/fragments/Page3Fragment.java
@@ -0,0 +1,27 @@
+package fr.doranco.flash.s1tp7.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import fr.doranco.flash.s1tp7.R;
+
+public class Page3Fragment extends Fragment {
+
+    public Page3Fragment() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_page3, container, false);
+    }
+}
